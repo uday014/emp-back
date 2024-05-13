4d7584dee0d951432f47090d7cfc6858757542b3
Uday Kumar <udayka014@gmail.com>
2024-05-13 20:49:51 +0530

    ulr var

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
+const mongodb_url =
+  "mongodb+srv://udayka014:uday123@cluster0.qycazkj.mongodb.net/training";
+
 mongoose.set("strictQuery", true);
 
-mongoose.connect(
-  "mongodb+srv://udayka014:uday123@cluster0.qycazkj.mongodb.net/training",
-  (err, docs) => {
-    if (!err) {
-      console.log("database connected");
-    } else {
-      console.log(err);
-    }
+mongoose.connect(mongodb_url, (err, docs) => {
+  if (!err) {
+    console.log("database connected");
+  } else {
+    console.log(err);
   }
-);
+});
 
 module.exports = mongoose;
